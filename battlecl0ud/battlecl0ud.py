8daf5f3f35805d6ee35795994a1cc4e9862459d2
tmc <1370650+khast3x@users.noreply.github.com>
2018-04-20 22:03:18 +0200

    clean local

--- battlecl0ud/battlecl0ud.py
+++ battlecl0ud/battlecl0ud.py
@@ -1,3 +1,29 @@
 # -*- coding: utf-8 -*-
+import ui
+import docker
+
+def main():
+    # coloring:
+    ui.info("This is", ui.red, "red",
+            ui.reset, "and this is", ui.bold, "bold")
+    ui.info(ui.check)
+
+    # enumerating:
+    list_of_things = ["foo", "bar", "baz"]
+    for i, thing in enumerate(list_of_things):
+        ui.info_count(i, len(list_of_things), thing)
+
+for i in range(10):
+    ui.info_progress("PROGRESSS", i, 10)
+if __name__ == '__main__':
+    # coloring:
+    ui.info("This is", ui.red, "red",
+            ui.reset, "and this is", ui.bold, "bold")
+    main()
+
+
+
+
+
 
 """Main module."""
